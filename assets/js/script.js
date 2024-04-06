1b780a7c02a71384034ade0b6493cc51c9486610
SmilyArt <voverdoterskij@gmail.com>
2024-04-06 13:57:36 +0300

    I added slick slider. This slider needed file slick.min.js, jquery-library and script.js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.routs__slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false
+    });
+});
